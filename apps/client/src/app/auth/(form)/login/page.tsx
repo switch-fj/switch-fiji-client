76c5aeb440b050ebda907fb756cda708bdcc0182
Techtees <taiwojoshuaoye@gmail.com>
2026-04-06 10:41:02 +0100

    Wire auth in client app

--- apps/client/src/app/auth/(form)/login/page.tsx
+++ apps/client/src/app/auth/(form)/login/page.tsx
@@ -1,1 +1,17 @@
-export { default } from '@switch-fiji/auth/views/LoginView';
+"use client";
+
+import LoginView from "@switch-fiji/auth/views/LoginView";
+import { useLogin, useVerifyLogin } from "@/hooks/useAuth";
+
+export default function ClientLoginPage() {
+  const { login, isLoading: isLoginLoading } = useLogin();
+  const { verifyLogin, isLoading: isVerifyLoading } = useVerifyLogin();
+
+  return (
+    <LoginView
+      login={login}
+      verifyLogin={verifyLogin}
+      isSubmitting={isLoginLoading || isVerifyLoading}
+    />
+  );
+}
